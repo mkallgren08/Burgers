5a7c25c459201723bb30ceda095e9709f995f279
Michael <mkallgren08@gmail.com>
2017-09-07 14:32:39 -0400

    Fixed the hadnlebars template path

--- app/server.js
+++ app/server.js
@@ -20,6 +20,7 @@ var expshbs = require("express-handlebars");
 
 app.engine("handlebars", expshbs({ defaultLayout : "main"}));
 app.set("view engine", "handlebars");
+app.set('views', path.join(__dirname, 'app/views'));
 
 var routes = require("./controllers/burgers_controller.js");
 
