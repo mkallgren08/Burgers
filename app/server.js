06f26805f43b061a2323b37418ada5ab65b5909c
Michael <mkallgren08@gmail.com>
2017-09-07 14:23:30 -0400

    Fixed the port

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 var path = require("path");
 
-var port = 8080;
+var port = process.env.PORT || 8080;
 
 var app = express();
 
